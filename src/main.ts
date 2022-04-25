3da033846a719df2803b47e8d116196c16b902be
リュウ・ショウウ <ryuushouu@ryuushouus-MacBook-Pro.local>
2022-04-25 11:25:14 +0900

    启动测试

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
+import { AppModule } from './app/app.module';
 // main app entry point
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 if (environment.production) {
   enableProdMode();
 }
-
+console.log('x');
 platformBrowserDynamic().bootstrapModule(AppModule);
